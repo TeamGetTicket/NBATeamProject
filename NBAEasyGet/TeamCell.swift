13b2d3a04065dd80853466a54dc5023ea3113e63
paul <paul@pauls-MacBook-Pro.local>
2018-10-18 15:30:53 -0700

    TeamView data complete

---
+++ NBAEasyGet/TeamCell.swift
@@ -0,0 +1,33 @@
+//
+//  TeamCell.swift
+//  NBAEasyGet
+//
+//  Created by paul on 10/17/18.
+//  Copyright © 2018 PoHung Wang. All rights reserved.
+//
+
+import UIKit
+
+class TeamCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var teamNameLabel: UILabel!
+    
+    @IBOutlet weak var teamAliasLabel: UILabel!
+    
+    @IBOutlet weak var teamVenueLabel: UILabel!
+    @IBOutlet weak var teamAddressLabel: UILabel!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
