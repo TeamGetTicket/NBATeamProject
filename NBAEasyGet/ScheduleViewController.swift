9f5bbd84ae7e8d9039f2a3287dced282077288bc
cjhong <cjhong6@gmail.com>
2018-10-26 22:27:14 -0700

    added schedule view controller

---
+++ NBAEasyGet/ScheduleViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ScheduleViewController.swift
+//  NBAEasyGet
+//
+//  Created by Chengjiu Hong on 10/24/18.
+//  Copyright © 2018 PoHung Wang. All rights reserved.
+//
+
+import UIKit
+
+class ScheduleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
